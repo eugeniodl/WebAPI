a852cb587d0c19992056de89cdc73013f9919c60
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-05-28 12:12:10 -0600

    Formulario WinForms

---
+++ WinFormsApp/UserForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WinFormsApp
+{
+    partial class UserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtUserName = new TextBox();
+            label3 = new Label();
+            txtEmail = new TextBox();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            dgvUsers = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvUsers).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(331, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(156, 48);
+            label1.TabIndex = 0;
+            label1.Text = "Usuarios";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(84, 74);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre:";
+            // 
+            // txtUserName
+            // 
+            txtUserName.Location = new Point(220, 71);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(389, 31);
+            txtUserName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(84, 128);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 25);
+            label3.TabIndex = 3;
+            label3.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(220, 125);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(389, 31);
+            txtEmail.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(84, 181);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(112, 34);
+            btnAdd.TabIndex = 5;
+            btnAdd.Text = "Agregar";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(283, 181);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(112, 34);
+            btnUpdate.TabIndex = 6;
+            btnUpdate.Text = "Modificar";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(497, 181);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(112, 34);
+            btnDelete.TabIndex = 7;
+            btnDelete.Text = "Eliminar";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // dgvUsers
+            // 
+            dgvUsers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvUsers.Location = new Point(39, 246);
+            dgvUsers.Name = "dgvUsers";
+            dgvUsers.ReadOnly = true;
+            dgvUsers.RowHeadersWidth = 62;
+            dgvUsers.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvUsers.Size = new Size(739, 247);
+            dgvUsers.TabIndex = 8;
+            // 
+            // UserForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(810, 526);
+            Controls.Add(dgvUsers);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
+            Controls.Add(txtEmail);
+            Controls.Add(label3);
+            Controls.Add(txtUserName);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "UserForm";
+            Text = "UserForm";
+            Load += UserForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvUsers).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUserName;
+        private Label label3;
+        private TextBox txtEmail;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private DataGridView dgvUsers;
+    }
+}
