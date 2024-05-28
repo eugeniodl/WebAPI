1f8d461309cc06b17d59032ac18a6683e639cf54
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-05-28 09:23:13 -0600

    WebAPI

--- Ejemplo02/Controllers/UsersController.cs
+++ Ejemplo02/Controllers/UsersController.cs
@@ -1,6 +1,5 @@
 ﻿using Ejemplo02.Data;
 using Ejemplo02.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,5 +22,75 @@ namespace Ejemplo02.Controllers
             return await _context.Users.ToListAsync();
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.UserId }, user);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if(id != user.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if(!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        private bool UserExists(int id)
+        {
+           return _context.Users.Any(e => e.UserId == id);
+        }
     }
 }
